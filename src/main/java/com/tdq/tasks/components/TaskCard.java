e84f360f77514efda4f63c068abb9848025f6886
Nikolai Gorokhov <nikolai@vaadin.com>
2017-11-30 20:42:13 +0200

    Refactoring

---
+++ src/main/java/com/tdq/tasks/components/TaskCard.java
@@ -0,0 +1,12 @@
+package com.tdq.tasks.components;
+
+import com.vaadin.flow.model.TemplateModel;
+import com.vaadin.ui.Tag;
+import com.vaadin.ui.common.HtmlImport;
+import com.vaadin.ui.polymertemplate.PolymerTemplate;
+
+@Tag("task-card")
+@HtmlImport("compoents/task-card.html")
+public class TaskCard extends PolymerTemplate<TemplateModel> {
+
+}
